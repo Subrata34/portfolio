5fe0c568b6b6d826c15e2f546182c4226d93b09f
Subrata34 <subratamodak3491@gmail.com>
2021-12-21 15:50:48 +0600

    files

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+
+
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+           <div>
+               <h1>I am subrata modak </h1>
+           </div>
+           <div>
+               <img src="" alt="" />
+           </div>
+        </div>
+    )
+}
+
+export default Home
