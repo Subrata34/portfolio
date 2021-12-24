9d2edabfedab9e3df018cd0d41198c8ce7d4dfc1
Subrata34 <subratamodak3491@gmail.com>
2021-12-24 12:19:27 +0600

    files

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,7 +3,8 @@ import React from 'react'
 const About = () => {
     return (
         <div>
-            
+            <h1>About Me</h1>
+            <h3>I am learn web develop.</h3>
         </div>
     )
 }
