20fb74835e1fd45051622968ceae0af6a0612349
Subrata34 <subratamodak3491@gmail.com>
2021-12-25 17:14:30 +0600

    files

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,29 +2,36 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div class="container">
-            <h1>Contact Me</h1>
+        <div style={{display:"flex"}}>
+            <div>
+             <h1> contact Me </h1>
+            </div>
+        <div class="container" style={{backgroundColor:"gray ",color:"black"}}>
+            <h1>Contact</h1>
              <form action="">
                  <div className="form-group">
-                     <label for="firstName">First Name</label>
+                     <label for="firstName" style ={{margin:"20px"}}>First Name:</label>
                      <input type="text" name="firstName" id="firstName" />
                  </div>
                  <div className="form-group">
-                     <label for="lastName">Last Name</label>
+                     <label for="lastName" style ={{margin:"20px"}} >Last Name :</label>
                      <input type="text" name="lastName" id="firstName" />
                  </div>
                  <div className="form-group">
-                     <label for="email">Email Address</label>
-                     <input type="email" name="email" id="email" />
+                     <label for="email" style ={{margin:"20px"}}>Email Address :</label>
+                     <input style={{marginRight:"10px"}} type="email" name="email" id="email" />
                  </div>
                  <div className="form-group">
-                 <label for="massage">Massage</label>
-                     <textarea name="massage" id="massage" cols="30" rows="10">
+                 <label for="massage" style ={{margin:"15px"}}>Massage :</label>
+                    
+                     <textarea name="massage" id="massage" cols="30" rows="5">
                      </textarea>
                  </div>
-                 <button type="submit">Submit</button>
+                 <button type="submit" style ={{margin:"20px",paddingRight:"40px",borderRadius:"15px",
+                  backgroundColor:"green",paddingLeft:"40px"}}>Submit</button>
                  
                 </form>            
+        </div>
         </div>
     )
 }
