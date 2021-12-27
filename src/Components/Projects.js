72fc3e9782d26094494a93317d6379e4eb3e57c0
Subrata34 <subratamodak3491@gmail.com>
2021-12-27 22:47:21 +0600

    files

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,10 +4,12 @@ import './Projects.css'
 const Projects = () => {
     return (
        <div>
-           <h1> Porjects</h1>
-           <div>
-               
-           </div>
+           <h1 style={{color:"black"}}> Porjects</h1>
+           
+               <div class="card">
+                   <img style={{clipPath:"circle()", width:"400px",marginLeft:"-75px"}} src="./image/image02.jpg" alt=""  />
+
+               </div>
        </div>
     )
 }
